6122523d44a778c261b0269b5e38522e40445275
Karl <tui98107@temple.edu>
2020-09-17 10:30:39 -0400

    add name edittext & confirm error

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -20,13 +20,19 @@ public class FormActivity extends AppCompatActivity {
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText name = findViewById(R.id.nameEditText);
                 EditText email = findViewById(R.id.emailEditText);
                 EditText password = findViewById(R.id.passwordEditText);
                 EditText confirm = findViewById(R.id.confirmEditText);
-                if (email.getText().toString().matches("") ||
+                if (name.getText().toString().matches("") ||
+                        email.getText().toString().matches("") ||
                         password.getText().toString().matches("") ||
                         confirm.getText().toString().matches("")) {
                     Toast.makeText(getApplicationContext(), "Enter all information",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if (!password.getText().toString().matches(confirm.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Passwords don't match",
                             Toast.LENGTH_SHORT).show();
                 }
             }
